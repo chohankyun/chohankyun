274c9023807cf70040de9f6b5ede2eb53e73693c
chohankyun <chohankyun@gmai.com>
2018-07-15 03:26:25 +0900

    내부 아이피 제거

--- chohankyun/settings.py
+++ chohankyun/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '21@6q5ny@wx+s02l067xboh@m!cikn#6mfa99-&&o+quux#p+f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
