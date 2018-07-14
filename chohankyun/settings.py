1dae810acd4931ee2a797d92e4f1c6b67943d21e
chohankyun <chohankyun@gmai.com>
2018-07-14 23:15:16 +0900

    임시 아이피 추가

--- chohankyun/settings.py
+++ chohankyun/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '21@6q5ny@wx+s02l067xboh@m!cikn#6mfa99-&&o+quux#p+f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '13.125.104.140', 'chohankyun.com', 'www.chohankyun.com']
+ALLOWED_HOSTS = ['127.0.0.1', '13.125.104.140', '172.31.19.140', 'chohankyun.com', 'www.chohankyun.com']
 
 # Application definition
 
