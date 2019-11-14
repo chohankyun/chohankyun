e5bac7be17cf80632149b23651c844fb59f6f7d0
chohankyun <chohankyun@gmail.com>
2019-11-14 13:16:15 +0900

    리펙토링 - back-end 다국어 처리   (이메일 다국어 처리, http response 처리 )

--- utils/serializers.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from rest_framework import serializers
-from django.templatetags import l10n
-
-
-class LocaleDateTimeField(serializers.DateTimeField):
-    def to_representation(self, value):
-        return l10n.localize(value)
